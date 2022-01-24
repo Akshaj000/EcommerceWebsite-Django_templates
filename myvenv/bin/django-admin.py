6b7cdd9cccd7eed00d0e452b5696e46b7af69944
Akshaj000 <akshajsr000@gmail.com>
2022-01-25 00:47:49 +0530

    Added paypal

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/1660E61660E5FC7D/AK360/DJANGO/ecommercewebsite/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
